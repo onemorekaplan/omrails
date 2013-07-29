328045d2d3451e5cf5655a4e2a784564adcc13de
Mike Kaplan <mike@mikekaplan.net>
2013-07-29 00:48:44 -0500

    Made app responsive on mobile devices and ensured all users have a first and last name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, #:recoverable,
+         :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
 
   # Fix for a recent update to devise.
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :firstname, :lastname, :as => [:default, :admin]
+
+  # Ensures that all users have a first and last name.
+  validates_presence_of :firstname
+  validates_presence_of :lastname
 end
